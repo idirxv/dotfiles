445ab1eac34a1a468fbaa0c8c38c9475ea8e4630
Idir Oura <idir.oura@protonmail.com>
2024-09-25 23:09:48 +0200

    cargo-packages: new script for cargo packages

---
+++ scripts/cargo-packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+. common-print.sh
+
+# This script will install the packages managed by cargo
+
+# Check if cargo is installed
+if ! command -v cargo &> /dev/null
+then
+    die "Cargo is not installed. Please install it first."
+fi
+
+packages=(
+    "lsd"
+    "bat"
+    "fd-find"
+    "ripgrep"
+)
+
+for package in "${packages[@]}"
+do
+    if cargo install $package
+    then
+        print_green "$package installed successfully"
+    else
+        print_red "Failed to install $package"
+    fi
+done
