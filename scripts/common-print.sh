c89faa4a80804f5eaba56a29b8932a7beaafe0cc
Idir Oura <idir.oura@protonmail.com>
2024-09-25 23:09:24 +0200

    common-print: add bash colored print functions

---
+++ scripts/common-print.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This will provide a common print function for all scripts
+
+print_green() {
+    echo -e "\e[32m$1\e[0m"
+}
+
+print_red() {
+    echo -e "\e[31m$1\e[0m"
+}
+
+print_yellow() {
+    echo -e "\e[33m$1\e[0m"
+}
+
+die () {
+    print_red "$1"
+    exit 1
+}
