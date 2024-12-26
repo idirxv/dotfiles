4026c4036ab8a427e3657b6f51fe6592ae677a76
Idir Oura <idir.oura@protonmail.com>
2024-12-26 15:48:36 +0100

    cargo-packages: add cargo-update

--- scripts/cargo-packages.sh
+++ scripts/cargo-packages.sh
@@ -5,25 +5,23 @@
 # This script will install the packages managed by cargo
 
 # Check if cargo is installed
-if ! command -v cargo &> /dev/null
-then
-    die "Cargo is not installed. Please install it first."
+if ! command -v cargo &>/dev/null; then
+  die "Cargo is not installed. Please install it first."
 fi
 
 packages=(
-    "lsd"
-    "bat"
-    "fd-find"
-    "ripgrep"
-    "zoxide"
+  "cargo-update"
+  "lsd"
+  "bat"
+  "fd-find"
+  "ripgrep"
+  "zoxide"
 )
 
-for package in "${packages[@]}"
-do
-    if cargo install $package
-    then
-        print_green "$package installed successfully"
-    else
-        print_red "Failed to install $package"
-    fi
+for package in "${packages[@]}"; do
+  if cargo install $package; then
+    print_green "$package installed successfully"
+  else
+    print_red "Failed to install $package"
+  fi
 done
